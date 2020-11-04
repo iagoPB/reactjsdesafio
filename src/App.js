567faf1ea3a11c21cdfb68148cc75530da5ae9d1
Iago Pereira <iagopereira72@gmail.com>
2020-11-04 01:12:45 -0300

    react

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, {useEffect, useState } from "react";
+import api from "./services/api";
+import "./styles.css";
+
+function App() {
+
+  const [repositories, setRepositories] = useState ([]);
+
+  useEffect (() => {
+
+    api.get ('repositories').then (response => {
+      setRepositories(response.data);
+
+    });
+
+  }, []);
+  async function handleAddRepository() {
+    // TODO
+    const response = await api.post ('repositories', {
+      title: 'Iago',
+      url: 'www.iago.com.br',
+      techs: ['Node.js']
+    })
+    setRepositories ([...repositories, response.data]);
+  }
+
+  async function handleRemoveRepository(id) {
+    // TODO
+    await api.delete (`repositories/${id}`);
+    setRepositories (repositories.filter(
+      repository => repository.id != id
+    ))
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (
+
+<li key = {repository.id} >
+  {repository.title}
+
+
+<button onClick={() => handleRemoveRepository(repository.id)}>
+  Remover
+</button>
+</li>
+
+        ))}
+        
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
